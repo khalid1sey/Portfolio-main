e3c154e968e7f1d5e813e234b969ef251d54ae25
khalid1sey <lilahseyfu@gmail.com>
2024-02-02 00:15:22 +0300

    added profile

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Test")
+// Toggle navigation on hamburger menu click
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.navigation ul').classList.toggle('active');
+  });
